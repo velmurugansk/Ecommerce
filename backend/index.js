afd4c58af1c76fe6d7789a1f7a744b5efadb8e10
skvelmurugan <skvelmurugan985@gmail.com>
2025-01-27 00:07:12 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,6 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
+console.log('hello')
 
 app.listen(port,()=> console.log(`server is running on port ${port}`));
