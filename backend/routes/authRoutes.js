46ab28732091f831b1d26dde45f1dd1a1c95676d
skvelmurugan <73580603+velmurugansk@users.noreply.github.com>
2025-03-30 17:27:52 +0530

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {adminLogin, getUser} = require('../controllers/authController');
-const {validateSession} = require('../middlewares/authMiddleware');
-
-router.post('/adminlogin', adminLogin);
-router.post('/getuser', validateSession,  getUser);
-
-module.exports = router
--- backend/utiles/response.js
+++
@@ -1,5 +0,0 @@
-const responseReturn = (res, code, data) => {
-    return res.status(code).json(data);
-}
-
-module.exports = {responseReturn};
